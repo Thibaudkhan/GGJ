0eacf999107eb04d6a234ee982bbfff1c49cf3e3
Thibaud <thibaud.roussel@edu.itescia.fr>
2020-02-01 17:02:17 +0100

    Add fight / Die

---
+++ GGJ20/Assets/Scripts/CharacterFight.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFight : MonoBehaviour
+{
+    public GameObject gameObject;
+    public Collider2D collider2D;
+
+    private bool isAttacking = false;
+    private float attackTimer = 0;
+    private float attackDmg = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider2D.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && !isAttacking)
+        {
+            isAttacking = true;
+            attackTimer = attackDmg;
+            collider2D.enabled = true;
+        }
+        if (isAttacking)
+        {
+            if(attackTimer > 0)
+            {
+                attackTimer -= Time.deltaTime;
+            }
+            else
+            {
+                isAttacking = false;
+                collider2D.enabled = false;
+            }
+        }
+            
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if (col.CompareTag("Pnj"))
+        {
+            Debug.Log("okayyy");
+        }
+    }
+    
+
+}
---
+++ GGJ20/Assets/Scripts/Weapons/WeaponController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public Collider2D collider2D;
+    public GameObject gameObject;
+    public GameObject gameObjectEn;
+    [SerializeField] private int damage;
+    private bool range;
+    CharacterController characterController;
+    CharacterController characterControllerEn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+        characterControllerEn = gameObjectEn.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Debug.Log("okayyy");
+        if (col.gameObject.name == "Pnj")
+        {
+            Debug.Log("okayyy"+ characterControllerEn.life);
+            characterControllerEn.life -= damage;
+
+            characterControllerEn.CheckIfDie(col.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("okayyy");
+        if (other.isTrigger != true && other.CompareTag("Pnj"))
+        {
+            
+            Debug.Log("okayyy");
+        }
+    }
+
+    /*public int receiveDamage(int dmg)
+    {
+
+        life -= dmg;
+        Debug.Log("my life " + life);
+        return life;
+    }*/
+}
