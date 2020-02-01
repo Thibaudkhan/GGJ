923002e83cb55726efa7b2ea6ad6bb85dd6c1e1b
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-02-01 02:03:08 +0100

    Movements

--- GGJ20/Assets/Scripts/playerScript.cs
+++ GGJ20/Assets/Scripts/playerScript.cs
@@ -6,15 +6,48 @@ using UnityEngine.InputSystem;
 public class playerScript : MonoBehaviour 
 {
 
+    [SerializeField] private LayerMask platformMask;
+    private Rigidbody2D body;
+    private BoxCollider2D boxCollider;
+    private SpriteRenderer sprite;
+
+    public float movementSpeed = 0.1f;
+    public float jumpForce = 30f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        body = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update() 
+    void FixedUpdate() 
     {
-        Debug.Log(Input.GetAxis("Horizontal"));
+        float hMovement = Input.GetAxis("Horizontal");
+
+        if (hMovement > 0)
+        {
+            transform.position += Vector3.right * movementSpeed;
+            sprite.flipX = true;
+
+        }
+        else if (hMovement < 0)
+        {
+            transform.position += Vector3.left * movementSpeed;
+            sprite.flipX = false;
+        }
+
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            body.AddForce(Vector3.up * jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D ray = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 1f, platformMask);
+        return ray.collider != null;
     }
 }
