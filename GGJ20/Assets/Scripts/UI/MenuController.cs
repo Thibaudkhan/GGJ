17c6aa0d2f6395790d27c0b4e582f25932e5958d
Thibaud <thibaud.roussel@edu.itescia.fr>
2020-02-02 13:29:17 +0100

    Add Menu UI

---
+++ GGJ20/Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    private static bool isPaused = false;
+
+    [SerializeField] private GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
