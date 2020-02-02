5528326d8d608cbc357e719540b48d9ada267aa5
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-02-02 11:11:00 +0100

    Fix

---
+++ GGJ20/Assets/NPC.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If the player press E
+
+        // Repair the equipped weapon
+
+        
+    }
+}
